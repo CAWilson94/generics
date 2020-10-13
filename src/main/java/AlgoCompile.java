e70a6ce23c975d86e46266494d1966f8f442592d
CAWilson94 <charlotteawilson.94@gmail.com>
2020-10-13 23:40:06 +0100

    Algo Compile Check

---
+++ src/main/java/AlgoCompile.java
@@ -0,0 +1,5 @@
+public final class  AlgoCompile {
+    public static <T> T max(T x, T y){
+        return x > y ? x : y; // basically won't compile as we cannot apply a logical operator to generic type
+    }
+}
